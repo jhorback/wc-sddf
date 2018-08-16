ce12a89498cf8e1c03644cf8af1bf30efc6b4e5e
John Horback <jhorback@gmail.com>
2018-08-15 20:31:43 -0400

    created the layout element

---
+++ src/common-elements/wc-sddf-layout/wc-sddf-layout.js
@@ -0,0 +1,38 @@
+import {LitElement, html} from "@polymer/lit-element/lit-element.js";
+import {style} from "./wc-sddf-layout-css.js";
+import "@polymer/app-layout/app-header/app-header.js";
+import "@polymer/app-layout/app-toolbar/app-toolbar.js";
+
+
+class WcSddfLayout extends LitElement {
+    static get properties() {
+        return {
+        };
+    }
+
+    constructor() {
+        super();
+    }
+
+    /**
+     * How to get users from the data element to set users here?
+     * @param {object} properties 
+     */
+    _render({}) {
+        return html`
+            ${style.content}
+            <app-header>
+                <app-toolbar>
+                    <div class="wc-org-logo"></div>
+                    <div main-title>WC-SDDF</div>
+                    <div class="connect-tech">CONNECT.TECH 2018</div>
+                </app-toolbar>
+                <app-toolbar class="bottom"></app-toolbar>
+            </app-header>
+            LAYOUT HERE 
+            <slot></slot>         
+        `;
+    }
+}
+
+customElements.define('wc-sddf-layout', WcSddfLayout);
--- src/wc-sddf-app/wc-sddf-app.js
+++ src/wc-sddf-app/wc-sddf-app.js
@@ -1,37 +1,13 @@
 import {LitElement, html} from '@polymer/lit-element/lit-element.js';
+import "../common-elements/wc-sddf-layout/wc-sddf-layout.js";
+
 
 class WcSddfApp extends LitElement {
-//class WcSddfApp extends HTMLElement {
-    static get properties() {
-        return {
-            foo: String,
-            state: Object
-        };
-    }
-
-    constructor() {
-        super();
-        this.state = {};
-        this.test();
-    }
-
-    async test() {
-        const foo = await this.testDeconstruction({foo:"bar"});
-        console.log("testing deconstruction:", foo);
-    }
-
-    async testDeconstruction(params) {
-        const {foo} = params;
-        return foo;
-    }
-
-    /**
-     * How to get users from the data element to set users here?
-     * @param {object} properties 
-     */
     _render({foo, state}) {
         return html`
-            <h1>WC-SDDF-APP</h1>           
+            <wc-sddf-layout>
+                <a href="example1.html">Example 1</a>
+            </wc-sddf-layout>
         `;
     }
 }
